ef090100829b1c2fc2c8dabd1fbe5e01db9f685f
andreamper220 <andrew-wolf68@yandex.ru>
2024-03-28 14:07:12 +0300

    wifi refactor

--- sketch_mar28b.ino
+++ sketch_mar28b.ino
@@ -1,12 +1,81 @@
+
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <SoftwareSerial.h>
 
-void setup(){
+const char* ssid = "cyberpunk";
+const char* password = "10011001";
+
+const char* serverName = "http://narodmon.ru/json";
+
+unsigned long lastTime = 0;
+unsigned long timerDelay = 360000;
+
+SoftwareSerial _serial(D5, D6);
+int _pm1, _pm25, _pm10;
+
+void setup() {
   Serial.begin(115200);
-  Serial.println();
-  Serial.print("ESP Board MAC Address:  ");
-  Serial.println(WiFi.macAddress());
-}
- 
-void loop(){
+  _serial.begin(9600);
 
+  WiFi.begin(ssid, password);
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+}
+
+void loop() {
+  if ((millis() - lastTime) > timerDelay) {
+    readSensor();
+
+    if(WiFi.status()== WL_CONNECTED){
+      WiFiClient client;
+      HTTPClient http;
+      
+      http.begin(client, serverName);
+      
+      http.addHeader("Content-Type", "application/json");
+
+      String json = "{\"devices\":[{\"mac\":\"E8DB84ED8FCF\",\"name\":\"mich1\",\"owner\":\"blini000\",\"lat\":52.888295,\"lon\":40.516813,\"alt\":152,\"sensors\":[{\"id\":\"PM1\",\"name\":\"PM2.5_1\",\"value\":" + _pm25;
+      int httpResponseCode = http.POST(json + ".00,\"unit\":\"\u00B5g\/m3\"}]}]}");
+        
+      http.end();
+    }
+    
+    lastTime = millis();
+  }
+}
+
+void readSensor() {
+  int checksum = 0;
+  unsigned char pms[32] = {0,};
+
+   Serial.println("Start");
+  while( _serial.available() && 
+      _serial.read() != 0x42 &&
+      _serial.peek() != 0x4D ) {
+  }    
+  if( _serial.available() >= 31 ){
+    pms[0] = 0x42;
+    checksum += pms[0];
+    for(int j=1; j<32 ; j++){
+      pms[j] = _serial.read();
+      if(j < 30)
+        checksum += pms[j];
+    }
+    _serial.flush();
+    if( pms[30] != (unsigned char)(checksum>>8) 
+      || pms[31]!= (unsigned char)(checksum) ){
+      Serial.println("Checksum error");
+      return;
+    }
+    if( pms[0]!=0x42 || pms[1]!=0x4d ) {
+      Serial.println("Packet error");
+      return;
+    }
+    _pm1  = makeWord(pms[10],pms[11]);
+    _pm25 = makeWord(pms[12],pms[13]);
+    _pm10 = makeWord(pms[14],pms[15]);
+  }   
 }
