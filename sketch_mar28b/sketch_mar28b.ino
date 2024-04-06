662613fd83cd2ad57b8f06b0ebe62270f84f5092
andreamper220 <andrew-wolf68@yandex.ru>
2024-04-06 12:05:48 +0300

    add new sensors

--- sketch_mar28b/sketch_mar28b.ino
+++ sketch_mar28b/sketch_mar28b.ino
@@ -3,6 +3,8 @@
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 #include <SoftwareSerial.h>
+#include <Adafruit_BME280.h>                            
+#include <Adafruit_Sensor.h>  
 
 const char* ssid = "cyberpunk";
 const char* password = "10011001";
@@ -10,13 +12,20 @@ const char* password = "10011001";
 const char* serverName = "http://narodmon.ru/json";
 
 unsigned long lastTime = 0;
-unsigned long timerDelay = 300000;
+unsigned long timerDelay = 5000;
+
+Adafruit_BME280 bme;
+float hum, temp, pres;
 
 SoftwareSerial _serial(D5, D6);
 int _pm1, _pm25, _pm10;
 
 void setup() {
   Serial.begin(115200);
+  bme.begin();
+  if (!bme.begin(0x76)) {                               
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+  }
   _serial.begin(9600);
 
   WiFi.begin(ssid, password);
@@ -33,37 +42,47 @@ void setup() {
 void loop() {
   if ((lastTime == 0) || ((millis() - lastTime) > timerDelay)) {
     readSensor();
-    Serial.printf("PM1.0:%d PM2.5:%d PM10.0:%d\n", _pm1, _pm25, _pm10);
+    Serial.printf("PM1.0 = %d\n PM2.5 = %d\n PM10.0 = %d\n", _pm1, _pm25, _pm10);
 
-    if(WiFi.status()== WL_CONNECTED){
-      WiFiClient client;
-      HTTPClient http;
-      
-      http.begin(client, serverName);
-      
-      http.addHeader("Content-Type", "application/json");
+    Serial.print("Temperature = ");
+    Serial.println(bme.readTemperature());
+    Serial.print("Humidity = ");
+    Serial.println(bme.readHumidity());
+    Serial.print("Pressure = ");
+    Serial.println(bme.readPressure());  
 
-      String json = 
-        String("{\"devices\":[{\"mac\":\"E8DB84ED8FCF\",\"name\":\"mich1\",\"owner\":\"blini000\",\"lat\":52.888295,\"lon\":40.516813,\"alt\":152,\"sensors\":[{\"id\":\"PM1\",\"name\":\"PM2.5_1\",\"value\":") 
-        + _pm25 
-        + String(".00,\"unit\":\"\u00B5g\/m3\"}]}]}");
-      
-      int httpResponseCode = http.POST(json);
-      if (httpResponseCode > 0) {
-        Serial.print("HTTP Response code: ");
-        Serial.println(httpResponseCode);
-        String payload = http.getString();
-        Serial.println(payload);
-      }
-      else {
-        Serial.print("Error code: ");
-        Serial.println(httpResponseCode);
-      }
-        
-      http.end();
-    } else {
-      Serial.println("WiFi Disconnected");
-    }
+    Serial.print("CO = ");
+    Serial.println(analogRead(A0));
+
+//    if(WiFi.status()== WL_CONNECTED){
+//      WiFiClient client;
+//      HTTPClient http;
+//      
+//      http.begin(client, serverName);
+//      
+//      http.addHeader("Content-Type", "application/json");
+//
+//      String json = 
+//        String("{\"devices\":[{\"mac\":\"E8DB84ED8FCF\",\"name\":\"mich1\",\"owner\":\"blini000\",\"lat\":52.888295,\"lon\":40.516813,\"alt\":152,\"sensors\":[{\"id\":\"PM1\",\"name\":\"PM2.5_1\",\"value\":") 
+//        + _pm25 
+//        + String(".00,\"unit\":\"\u00B5g\/m3\"}]}]}");
+//      
+//      int httpResponseCode = http.POST(json);
+//      if (httpResponseCode > 0) {
+//        Serial.print("HTTP Response code: ");
+//        Serial.println(httpResponseCode);
+//        String payload = http.getString();
+//        Serial.println(payload);
+//      }
+//      else {
+//        Serial.print("Error code: ");
+//        Serial.println(httpResponseCode);
+//      }
+//        
+//      http.end();
+//    } else {
+//      Serial.println("WiFi Disconnected");
+//    }
     
     lastTime = millis();
   }
