221492b68075b295667ebff6d3ad62d656d1cadc
unknown <porotikovivan9@gmail.com>
2024-04-20 10:44:59 +0300

    add some new

--- sketch_mar28b/sketch_mar28b.ino
+++ sketch_mar28b/sketch_mar28b.ino
@@ -53,46 +53,43 @@ void setup() {
 void loop() {
   if ((lastTime == 0) || ((millis() - lastTime) > timerDelay)) {
     readSensor();
-    Serial.printf("PM1.0 = %d\n PM2.5 = %d\n PM10.0 = %d\n", _pm1, _pm25, _pm10);
-    Serial.print("Temperature = ");
-    Serial.println(dht.readTemperature(),2);
-    Serial.print("Humidity = ");
-    Serial.println(dht.readHumidity(),2);
-    Serial.print("Pressure = ");
-    Serial.println(bmp.readPressure() * 0.0075);  
 
-    Serial.print("CO = ");
-    Serial.println(analogRead(A0));
+    if(WiFi.status()== WL_CONNECTED){
+      WiFiClient client;
+      HTTPClient http;
+      
+      http.begin(client, serverName);
+      
+      http.addHeader("Content-Type", "application/json");
 
-//    if(WiFi.status()== WL_CONNECTED){
-//      WiFiClient client;
-//      HTTPClient http;
-//      
-//      http.begin(client, serverName);
-//      
-//      http.addHeader("Content-Type", "application/json");
-//
-//      String json = 
-//        String("") 
-//        + _pm25 
-//        + String(".00,\"unit\":\"\u00B5g\/m3\"}]}]}");
-//      
-//      int httpResponseCode = http.POST(json);
-//      if (httpResponseCode > 0) {
-//        Serial.print("HTTP Response code: ");
-//        Serial.println(httpResponseCode);
-//        String payload = http.getString();
-//        Serial.println(payload);
-//      }
-//      else {
-//        Serial.print("Error code: ");
-//        Serial.println(httpResponseCode);
-//      }
-//        
-//      http.end();
-//    } else {
-//      Serial.println("WiFi Disconnected");
-//    }
+      String json = 
+        String("{\"devices\":[{\"mac\":\"E8DB84ED8FCF\",\"name\":\"mich_1\",\"owner\":\"blini000\",\"lat\":52.888295,\"lon\":40.516813,\"alt\":291,\"sensors\":[{\"id\":\"PM1\",\"name\":\"PM2.5_1\",\"value\":"
+          + _pm25 
+          + ",{\"unit\":\"u00B5g\/m3\"},{\"id\":\"CO1\",\"name\":\"CO_1\",\"value\":"
+          + analogRead(A0)
+          + ",\"unit\":\"ppm\"},{\"id\":\"TEMP\",\"name\":\"TEMP_1\",\"value\":"
+          + dht.readTemperature()
+          + ",\"unit\":\"C\"},{\"id\":\"HUM1\",\"name\":\"HUM_1\",\"value\":"
+          + dht.readHumidity()
+          + ",\"unit\":\"%\"},{\"id\":\"PRES1\",\"name\":\"PRES_1\",\"value\":"
+          + bmp.readPressure()
+          + ",\"unit\":\"mmHg\"}]}]}");      
+      int httpResponseCode = http.POST(json);
+      if (httpResponseCode > 0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        String payload = http.getString();
+        Serial.println(payload);
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+        
+      http.end();
+    } else {
+      Serial.println("WiFi Disconnected");
+    }
     
     lastTime = millis();
   }
